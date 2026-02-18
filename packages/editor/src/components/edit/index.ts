3af067b4969973b01752b477a69f32250963a2c9
Jacob Sampson <jsampson@atlassian.com>
2026-02-18 10:28:43 -0600

    Extract patchwork repo

--- packages/editor/src/components/edit/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './types';
-export * from './api';
-export * from './useEditSession';
-export * from './EditHistory';
-export * from './EditModal';
-export * from './FileTree';
--- packages/editor/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { CodeBlockExtension } from './CodeBlockExtension';
-export { CodePreview } from './CodePreview';
-export { MarkdownEditor } from './MarkdownEditor';
-export { ServicesInspector, type ServiceInfo } from './ServicesInspector';
-export * from './edit';
